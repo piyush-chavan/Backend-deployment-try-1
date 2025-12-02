9bae50b341f57fd9029b0ff4fcc3824c93095a5f
Piyush_Chavan <piyushchavangd2004@gmail.com>
2025-12-03 00:28:24 +0530

    Post api added for users route

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+let users = [
+    { id: 1, name: "Piyush" },
+    { id: 2, name: "Raj" }
+  ]
+
 app.get("/",(req,res)=>{
     res.json({message:"Backend server is running Properly !"})
 })
 
 app.get("/users",(req,res)=>{
-    res.json([
-    { id: 1, name: "Piyush" },
-    { id: 2, name: "Raj" }
-  ]);
+    res.json(users);
+})
+
+app.post("/users",(req,res)=>{
+    const body = req.body
+    users.push(body)
+    if(body){
+        res.send("Data added !")
+    }
 })
 
 const PORT = process.env.PORT || 3030
