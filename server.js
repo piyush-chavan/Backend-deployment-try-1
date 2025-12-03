156b90d2c6050065ae399f21bfcc0a49d3d016f2
Piyush_Chavan <piyushchavangd2004@gmail.com>
2025-12-04 02:43:09 +0530

    Chatting basic version added

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ let users = [
     { id: 2, name: "Raj" }
   ]
 
+let messages = [
+    {user:"demo_user",user_id: 1764788092871 ,msg:"Hi there !",time:1764788092871}
+]
+
 app.get("/",(req,res)=>{
     res.json({message:"Backend server is running Properly !"})
 })
@@ -19,11 +23,23 @@ app.get("/users",(req,res)=>{
     res.json(users);
 })
 
+app.get("/messages",(req,res)=>{
+    res.json(messages)
+})
+
 app.post("/users",(req,res)=>{
     const body = req.body
     users.push(body)
     if(body){
         res.send("Data added !")
+    }
+})
+
+app.post("/messages",(req,res)=>{
+    const body = req.body
+    messages.push(body)
+    if(body){
+        res.send("Message Sent !")
     }
 })
 
